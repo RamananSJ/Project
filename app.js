adc084fdc685ea75403306c2f377c1057144ee98
RamananSJ <127139413+RamananSJ@users.noreply.github.com>
2025-03-21 00:49:48 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,69 @@
+const stompClient = new StompJs.Client({
+    brokerURL: 'ws://localhost:8080/gs-guide-websocket'
+});
+
+stompClient.onConnect = (frame) => {
+    setConnected(true);
+    console.log('Connected: ' + frame);
+    stompClient.subscribe('/topic/greetings', (greeting) => {
+        showGreeting(JSON.parse(greeting.body).content);
+    });
+};
+
+stompClient.onWebSocketError = (error) => {
+    console.error('Error with websocket', error);
+};
+
+stompClient.onStompError = (frame) => {
+    console.error('Broker reported error: ' + frame.headers['message']);
+    console.error('Additional details: ' + frame.body);
+};
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    stompClient.activate();
+}
+
+function disconnect() {
+    stompClient.deactivate();
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function showGreeting(message, isMine) {
+    let messageClass = isMine ? "my-message" : "other-message";
+    let messageElement = `<div class="message ${messageClass}">${message}</div>`;
+    $("#conversation").append(messageElement);
+}
+
+// Send Message
+function sendName() {
+    let message = $("#name").val();
+    if (!message.trim()) return;
+
+    stompClient.publish({
+        destination: "/app/hello",
+        body: JSON.stringify({'name': message})
+    });
+
+    showGreeting(message, true); // Show my message
+    $("#name").val(""); // Clear input field
+}
+
+$(function () {
+    $("form").on('submit', (e) => e.preventDefault());
+    $( "#connect" ).click(() => connect());
+    $( "#disconnect" ).click(() => disconnect());
+    $( "#send" ).click(() => sendName());
+});
